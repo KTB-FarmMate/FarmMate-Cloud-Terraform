08c32515dd2ecc5064a343f7ac88d4356ed5aa40
WWvision <rlatmdwn31@gmail.com>
2024-12-04 20:13:31 +0900

    add ec2 key

--- modules/ec2_instance/main.tf
+++ modules/ec2_instance/main.tf
@@ -3,7 +3,8 @@ resource "aws_instance" "this" {
   instance_type          = var.instance_type
   subnet_id              = var.subnet_id
   vpc_security_group_ids = var.security_group_ids
-  
+  key_name               = "farmmate-cloud"
+
   root_block_device {
     volume_size = var.volume
     volume_type = "gp3"
